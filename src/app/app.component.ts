36b549f2e33739519e31004b8d91e167169dee8a
carlosmartin667 <46163584+carlosmartin667@users.noreply.github.com>
2022-10-11 09:06:19 -0300

    iniciando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend-usuario';
+  title: string = 'hola mundo';
+  contador: number = 0;
+
+  aumentar() {
+    this.contador++;
+  }
+  dismunir() {
+    this.contador--;
+  }
 }
